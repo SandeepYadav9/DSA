e17590de6d7075cefd12bd2b29f9797cb371ec99
Sandeep Yadav <81950280+SandeepYadav9@users.noreply.github.com>
2022-06-21 18:52:32 +0530

    Work in Progress...

---
+++ BST.java
@@ -0,0 +1,42 @@
+/* What is a BST ? 
+*  Binary Tree 
+// Every feeture of Binary tree 
+
+
+a. Left subtree Nodes < Root
+b. Right subtree Nodes > Root
+c. Left & Right Subtrees are also 
+   BST with no dublicates
+
+Special Property
+Inorder Traversal of BST gives a sorted sequence 
+// PreeOrder - Root LS RS
+// Inorder- LS ROOT RS
+// PostOrder - LS RS ROOT
+
+
+
+
+
+*/
+
+public class BST {
+
+   static class Node {
+      int data;
+      Node Left;
+      Node right;
+      Node(int data){
+         this.data =data;
+      }
+   }
+   public static Node insert(Node root, int val){
+      if(root == null){
+         
+      }
+   }
+   public static void main(String args[]){
+      int values[]={5,1,3,4,2,7}
+      Node root =null;
+   }
+}
