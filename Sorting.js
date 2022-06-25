8fe2b5bff4a9448b971a6278ba986dfb17143e6d
Sandeep Yadav <81950280+SandeepYadav9@users.noreply.github.com>
2022-06-25 17:19:51 +0530

    Update Sorting.js

--- Sorting.js
+++ Sorting.js
@@ -1,9 +1,12 @@
 //Bubble Sorts
 
+ // Time complexcity = B-O(n^2)
+
 function bubbleSort(arr) {
-  for (let i = 0; i < arr.length - 1; i++) {
-    for (let j = 0; j < arr.length - 1 - i; j++) {
+  for (let i = 0; i < arr.length - 1; i++) {          // loop for n-1 iteration count 
+    for (let j = 0; j < arr.length - 1 - i; j++) {    // first iteration one element sorted 
       if (arr[j] > arr[j + 1]) {
+        // Swiping of two number
         let temp = arr[j];
         arr[j] = arr[j + 1];
         arr[j + 1] = temp;
@@ -18,15 +21,17 @@ console.log(bubbleSort(arr));
 
 
 //Selection Sort
+// Time complexcity of B-O(n^2);
 
 function selectionSort(arr) {
-  for (let i = 0; i < arr.length - 1; i++) {
-    let smalest = i;
-    for (let j = i + 1; j < arr.length; j++) {
+  for (let i = 0; i < arr.length - 1; i++) { // n iteration count loop
+    let smalest = i;  // 
+    for (let j = i + 1; j < arr.length; j++) { // 
       if (arr[smalest] > arr[j]) {
         smalest = j;
       }
     }
+    // Swiping 
     let temp = arr[smalest];
     arr[smalest] = arr[i];
     arr[i] = temp;
@@ -40,20 +45,22 @@ console.log(selectionSort(arr));
 
 // inserson Sort
 /*  
-  [7,8,5,3,2,1]
-   s/u
+  [7,|       8,5,3,2,1]
+ sortedArr , unSortedArr
 
   */
 
+// Time complexcity of B-O(n);
+
 function insersonSort(arr) {
-  for (let i = 1; i < arr.length; i++) {
-    let currentElement = arr[i];
-    let j = i - 1;
-    while (j >= 0 && currentElement < arr[j]) {
+  for (let i = 1; i < arr.length; i++) {  // n iteration for unsorted arr
+    let currentElement = arr[i]; // unsorted first element
+    let j = i - 1;  // sortedArr index 
+    while (j >= 0 && currentElement < arr[j]) {  // Compaire 
       arr[j + 1] = arr[j];
-      j--;
+      j--; // Making place for sortdArr
     }
-    arr[j + 1] = currentElement;
+    arr[j + 1] = currentElement; 
   }
   return arr;
 }
